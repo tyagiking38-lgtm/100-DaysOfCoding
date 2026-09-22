067a0cf0aa1ae82280df7437eaa5ca63db7bccd5
Aditya Tyagi <tyagiking38@gmail.com>
2026-09-23 01:54:15 +0530

    DAY1

---
+++ P1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a,b,sum;
+    printf("enter first number:");
+    scanf("%d",&a);
+    printf("enter second number:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("sum of %d and %d is %d",a,b,sum);
+    return 0;
+}
